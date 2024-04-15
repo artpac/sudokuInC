d15e52237c61ad52be25f15713dca1ec5de174de
Artur Pacut <arturpacut904@gmail.com>
2024-04-15 15:49:06 +0100

    Created inputValues function

--- sudokuGame.c
+++ sudokuGame.c
@@ -7,17 +7,28 @@
 
 //Creating board Struct with all the array names and sizes
 struct board{
-    int x1[9];
-    int x2[9];
-    int x3[9];
-    int x4[9];
-    int x5[9];
-    int x6[9];
-    int x7[9];
-    int x8[9];
-    int x9[9];
+    int y1[9];
+    int y2[9];
+    int y3[9];
+    int y4[9];
+    int y5[9];
+    int y6[9];
+    int y7[9];
+    int y8[9];
+    int y9[9];
+
+    int y1Not[9];
+    int y2Not[9];
+    int y3Not[9];
+    int y4Not[9];
+    int y5Not[9];
+    int y6Not[9];
+    int y7Not[9];
+    int y8Not[9];
+    int y9Not[9];
 };
 typedef struct board Board;
+struct board currentBoard;
 
 //Board difficulty
 struct board easyBoard(){
@@ -30,42 +41,108 @@ struct board easyBoard(){
                                     {6,1,0,0,0,0,0,2,4}, 
                                     {5,9,1,8,4,7,6,0}, 
                                     {0,0,0,0,5,7,0,0,1}, 
-                                    {0,6,4,0,9,3,2,8,5}};
+                                    {0,6,4,0,9,3,2,8,5},
+                                    
+                                    {1,2,4,7},
+                                    {2,3,4,5},
+                                    {5,9},
+                                    {5,6,7,8},
+                                    {3,4},
+                                    {1,2,8,9},
+                                    {1,2,3,4,5,6,7,8},
+                                    {5,6,9},
+                                    {2,3,5,6,7,8,9}};
 
     return easyBoardStruct;
 }
 
 
 //Declaring menu function so it can be called before it is coded
-int menu(void);
+void menu(struct board currentBoard);
+void displayBoard(struct board displayCurrentBoard);
+void writeToFile();
+
 
 //All necessary functions
-int inputValue(void){
-    printf("Enter a value \n");
-    menu();
-    return 0;
+struct board inputValue(struct board currentBoard){
+    int x;
+    int y;
+    int newValue;
+    
+    
+    printf("Enter y coordinate \n");
+    scanf("%d", &y);
+    writeToFile(y);
+    printf("Enter x coordinate \n");
+    scanf("%d", &x);
+    writeToFile(x);
+    int length = sizeof(currentBoard.y1Not) / sizeof(currentBoard.y1Not[0]);
+    
+    printf("Enter new value \n");
+    scanf("%d", &newValue);
+    writeToFile(newValue);
+
+    if(y == 1){
+        
+        
+
+        // for (int i = 0; i < sizeof(currentBoard.y1Not); i++){
+        //     if(x == currentBoard.y1Not[i]){
+        //         printf("Cannot enter value there enter: Enter another value\n");
+        //         scanf("%d", &x);
+        //     }
+        // }
+        
+
+        currentBoard.y1[x-1] = newValue;
+    }
+    else if(y == 2){
+        currentBoard.y2[x-1] = newValue;
+    }
+    else if(y == 3){
+        currentBoard.y3[x-1] = newValue;
+    }
+    else if(y == 4){
+        currentBoard.y4[x-1] = newValue;
+    }
+    else if(y == 5){
+        currentBoard.y5[x-1] = newValue;
+    }
+    else if(y == 6){
+        currentBoard.y6[x-1] = newValue;
+    }
+    else if(y == 7){
+        currentBoard.y7[x-1] = newValue;
+    }
+    else if(y == 8){
+        currentBoard.y8[x-1] = newValue; 
+    }
+    else if(y == 9){
+        currentBoard.y9[x-1] = newValue;
+    }
+
+    displayBoard(currentBoard);
+    return currentBoard;
 }
-int undo(void){
+int undo(){
     printf("Undo \n");
-    menu();
+
     return 0;
 }
-int redo(void){
+int redo(){
     printf("Redo \n");
-    menu();
+
     return 0;
 }
-int delete(void){
-    printf("delete \n");
-    menu();
-    return 0;
+void delete(struct board currentBoard){
+    
 }
 int quit(){
     printf("quit \n");
     exit(1);
 
 }
-int isSolved(void){
+int isSolved(){
     printf("Is the board solved");
     return 0;
 }
@@ -74,135 +151,135 @@ int timeLimit(void){
     return 0;
 }
 
-int writeToFile(input){
+void writeToFile(input){
     FILE *file;
     file = fopen("savegame.txt", "a");
     fprintf(file, "%d", input);
     fclose(file);
 
-    return 0;
 }
-int displayBoard(Board displayCurrentBoard){
+void displayBoard(struct board displayCurrentBoard){
     // char currentX[] = "xX";
     // for(int value = 0; value < 9; value++){
     //     currentX[1] = value + '0';
     // }
-    printf("\n---------------------------------------\n|");
-    printf("| ");
+
+    printf("\n------------------------------------\n|");
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x1[i] == 0){
+        if(displayCurrentBoard.y1[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x1[i] != 0){
-            printf("%d | ", displayCurrentBoard.x1[i]);
+        if(displayCurrentBoard.y1[i] != 0){
+            printf("%d | ", displayCurrentBoard.y1[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x2[i] == 0){
+        if(displayCurrentBoard.y2[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x2[i] != 0){
-            printf("%d | ", displayCurrentBoard.x2[i]);
+        if(displayCurrentBoard.y2[i] != 0){
+            printf("%d | ", displayCurrentBoard.y2[i]);
+        }
+        
+    }
+    printf("\n------------------------------------\n|");
+
+    for(int i = 0; i < 9; i++){
+        if(displayCurrentBoard.y3[i] == 0){
+            printf("  | ");
+        }
+        if(displayCurrentBoard.y3[i] != 0){
+            printf("%d | ", displayCurrentBoard.y3[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x3[i] == 0){
+        if(displayCurrentBoard.y4[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x3[i] != 0){
-            printf("%d | ", displayCurrentBoard.x3[i]);
+        if(displayCurrentBoard.y4[i] != 0){
+            printf("%d | ", displayCurrentBoard.y4[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x4[i] == 0){
+        if(displayCurrentBoard.y5[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x4[i] != 0){
-            printf("%d | ", displayCurrentBoard.x4[i]);
+        if(displayCurrentBoard.y5[i] != 0){
+            printf("%d | ", displayCurrentBoard.y5[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x5[i] == 0){
+        if(displayCurrentBoard.y6[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x5[i] != 0){
-            printf("%d | ", displayCurrentBoard.x5[i]);
+        if(displayCurrentBoard.y6[i] != 0){
+            printf("%d | ", displayCurrentBoard.y6[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x6[i] == 0){
+        if(displayCurrentBoard.y7[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x6[i] != 0){
-            printf("%d | ", displayCurrentBoard.x6[i]);
+        if(displayCurrentBoard.y7[i] != 0){
+            printf("%d | ", displayCurrentBoard.y7[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x7[i] == 0){
+        if(displayCurrentBoard.y8[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x7[i] != 0){
-            printf("%d | ", displayCurrentBoard.x7[i]);
+        if(displayCurrentBoard.y8[i] != 0){
+            printf("%d | ", displayCurrentBoard.y8[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
     for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x8[i] == 0){
+        if(displayCurrentBoard.y9[i] == 0){
             printf("  | ");
         }
-        if(displayCurrentBoard.x8[i] != 0){
-            printf("%d | ", displayCurrentBoard.x8[i]);
+        if(displayCurrentBoard.y9[i] != 0){
+            printf("%d | ", displayCurrentBoard.y9[i]);
         }
 
     }
-    printf("\n---------------------------------------\n|");
+    printf("\n------------------------------------\n|");
 
-    for(int i = 0; i < 9; i++){
-        if(displayCurrentBoard.x9[i] == 0){
-            printf("  | ");
-        }
-        if(displayCurrentBoard.x9[i] != 0){
-            printf("%d | ", displayCurrentBoard.x9[i]);
-        }
-
-    }
-    printf("\n---------------------------------------\n|");
-    return 0;
 }
 
 
 //Creating menu function
-int menu(void){
+void menu(struct board currentBoard){
     int choice;
-    printf("\n1) Input Value\n2) Undo\n3) Redo\n4) Delete\n5) Quit ");
+    printf("\n1) Input Value\n2) Undo\n3) Redo\n4) Delete\n5) Display Board\n6) Quit ");
     scanf("%d", &choice);
     writeToFile(choice);
-    while(choice < 1 || choice > 5 ){
+    while(choice < 1 || choice > 6 ){
         printf("Invalid: Enter another value ");
         scanf("%d", &choice);   
     }
     if (choice == 1){
-        inputValue();
+        currentBoard = inputValue(currentBoard);
+        menu(currentBoard);
     }
     else if(choice == 2){
         undo();
@@ -211,38 +288,33 @@ int menu(void){
         redo();
     }
     else if (choice == 4){
-        delete();
+        // currentBoard = delete(currentBoard);
+        menu(currentBoard);
     }
     else if(choice == 5){
+        displayBoard(currentBoard);
+        menu(currentBoard);
+    }
+    else if(choice == 6){
         quit();
     }
-    return 0;
 }
 
 
 
-int main(void){
+int main(){
     //Created file pointer and started a new savegame.txt file
     FILE *file;
     file = fopen("savegame.txt", "w");
     //Created board struct to chose level;
     struct board selectedBoard;
-
+    // currentBoard = selectedBoard;
 
     printf("hello\n");
-    int functionCall;
-    printf("Enter your choice");
-    scanf("%d", &functionCall);
-    writeToFile(functionCall);
-    printf("You entered %d \n", functionCall);
 
-    if(functionCall == 1){
-        menu();
-    }
-    if(functionCall == 2){
-        selectedBoard = easyBoard();
-    }
+    selectedBoard = easyBoard();
     displayBoard(selectedBoard);
+    menu(selectedBoard);
 
     
 }
